1ff843df1c9f2f4b65b17ba48206b8b9b1678ebb
Finn <f.beardmore@uqconnect.edu.au>
2021-08-20 10:42:46 +1000

    Test Commit/Framework for Form Handling
    Please feel free to remove the commit, fingers crossed it doesn't break the build.

---
+++ src/services/formService.js
@@ -0,0 +1,16 @@
+
+/**
+ * A service for compiling the form required for either Practioner or User 
+ */
+ class FormService  {
+    /**
+     * Compiles the forms required from the form package 
+     * @returns The required forms for the users  
+     */
+    static fetchForm = async () => {
+      
+    };
+  }
+  
+  export default formService;
+  
