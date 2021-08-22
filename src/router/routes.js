16f1dba3fb0ee7b327ecf2eba62b97f3898c8e97
Finn <f.beardmore@uqconnect.edu.au>
2021-08-22 12:21:59 +1000

    Added Formik and Form handler
    Adding the formik library and form handler page at /form

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,9 +1,11 @@
+import FormHandler from "@features/FormHandler/FormHandler";
 import UserHeaderExample from "@features/UserHeaderExample/UserHeaderExample";
 
 // '/' usually goes to some home or landing page, going to example for now.
 const routes = [
   { path: "/", component: UserHeaderExample, private: false },
   { path: "/example", component: UserHeaderExample, private: false },
+  { path: "/form", component: FormHandler, private: false },
 ];
 
 export default routes;
